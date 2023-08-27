cd030e683a7e744b9001ada3044fc959506f7b0e
KingJ89 <janmutewera@gmail.com>
2023-08-27 15:49:28 -0700

    task 0 amended

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,14 +15,15 @@ size_t print_list(const list_t *h)
 
 {
 	size_t jm1 = 0;
+	char str= 0;
 
 	while (h)
 	{
-		if (!h->str)
+		if (!h -> str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
+			printf("[%u] %s\n", h -> len, h -> str);
+		h = h -> next;
 	jm1++;
 	}
 	return (jm1);
