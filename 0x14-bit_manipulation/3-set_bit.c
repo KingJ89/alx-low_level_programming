a8256d1110d0406c04358b98c8b6e859d8fb2e43
KingJ89 <janmutewera@gmail.com>
2023-09-03 11:18:51 -0700

    task 3 (set bit) completed

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,17 +8,12 @@
  *
  * Return: 0 for success, 1 on error
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	{
+
 	if (index > 63)
-		return (1);
-	}
-
-	{
-
+	return (1);
 	*n = ((1UL << index) | *n);
-	
 	return (0);
-	}
 }
