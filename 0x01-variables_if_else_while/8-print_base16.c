e9408fd609d1bacbf3e45f7b26c873d311a2e73f
KingJ89 <janmutewera@gmail.com>
2023-09-10 06:43:31 -0700

    task 8 second amend

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,10 +5,11 @@
  *
  * Return: return always 0
  */
+int main(void)
+
 {
 	int jm;
 	char jm1;
-
 	{
 		for (jm = 0 ; jm < 10 ; jm++)
 			putchar(jm + '0');
@@ -17,6 +18,6 @@
 		for (jm1 =  'a' ; jm1 <= 'f' ; jm1++)
 			putchar(jm1);
 	}
-	putchar('\n);
+	putchar('\n');
 	return (0);
 }
