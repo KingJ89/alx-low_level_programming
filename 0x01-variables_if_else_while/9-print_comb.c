1c99463b7b800689f1d3fc6aa962645f6c1dd65c
KingJ89 <janmutewera@gmail.com>
2023-09-10 11:54:10 -0700

    task 9 completed successfully

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int jm;
+
+	for (jm = 0 ; jm < 10 ; jm++)
+	{
+		putchar(jm);
+		putchar(',');
+		putchar(' ');
+	}
+		return (0);
+}
