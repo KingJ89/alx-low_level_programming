0a4bece0e83893362fddb4a09b6e4e8a55160970
KingJ89 <janmutewera@gmail.com>
2023-09-10 05:56:38 -0700

    task 7 completed successfully

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Main  Entry point
+ *
+ * Return: return always 0
+ */
+
+int main(void)
+{
+
+	char jm;
+
+	{
+	for (jm = 'z' ; jm >= 'a' ; jm--)
+		putchar(jm);
+	putchar('\n');
+	}
+	return (0);
+}
