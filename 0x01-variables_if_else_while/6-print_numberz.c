72123faf3045d5d4f8eb4953b9c8bfa2704ac68d
KingJ89 <janmutewera@gmail.com>
2023-09-10 05:24:16 -0700

    task 6 completed successfully

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entrypoint
+ *
+ * Return always 0
+ */
+int main(void)
+{
+	int jm;
+	{
+	for (jm = 0 ; jm < 10 ; jm++)
+		putchar (jm + 0);
+	}
+	putchar('\n');
+	return (0);
+}
+
