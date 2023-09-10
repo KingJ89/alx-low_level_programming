fc263a9c53265cdc161b939ecb19b6999da6adc9
KingJ89 <janmutewera@gmail.com>
2023-09-10 06:30:35 -0700

    task 8 completed successfully

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: return always 0
+ */
+{
+	int jm;
+	char jm1;
+
+	{
+		for (jm = 0 ; jm < 10 ; jm++)
+			putchar(jm + '0');
+	}
+	{
+		for (jm1 =  'a' ; jm1 <= 'f' ; jm1++)
+			putchar(jm1);
+	}
+	putchar('\n);
+	return (0);
+}
