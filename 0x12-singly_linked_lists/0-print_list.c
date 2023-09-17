74bbad77a62da539db9f221f056fde255013d9dd
KingJ89 <janmutewera@gmail.com>
2023-09-16 22:16:20 -0700

    task 0 fina final commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,9 +6,6 @@
 /**
  *print_list - prints elements of a linked list
  *@h: pointer to list_t
- *@next: pointer
- *@str: string literal
- *@len: length
  *Return: number of nodes
  */
 
@@ -17,15 +14,13 @@ size_t print_list(const list_t *h)
 	size_t jm1 = 0;
 
 	while (h)
-	{	
+	{
 	if (!h->str)
 		printf("[0] (nil)\n");
-	
 	else
 		printf("[%u] %s\n", h->len, h->str);
 	}
 		h = h->next;
 		jm1++;
-
 	return (jm1);
 }
