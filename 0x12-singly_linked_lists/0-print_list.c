1ff322f57d3173702b1ca390c826b97ed6332b31
KingJ89 <janmutewera@gmail.com>
2023-09-16 22:58:03 -0700

    task 0 13th amendment

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,12 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
+
 
 /**
  *print_list - prints elements of a linked list according to params
  *@h: pointer to list
  *Return: number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t jm1 = 0;
