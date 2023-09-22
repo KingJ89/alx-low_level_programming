48586249f59547a84489bf0541eef04fda4060e8
KingJ89 <janmutewera@gmail.com>
2023-09-22 03:19:20 -0700

    task 0 file i/o amended

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+/**
+ * main.h file for the 0x15 file io project by Jan Y Mutewera.
+ 
+ */
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
+
