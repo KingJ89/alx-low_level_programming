c1058f0530c08106a8a26773f302288f56702d83
KingJ89 <janmutewera@gmail.com>
2023-09-28 12:07:41 -0700

    task 0 and 1 completed successfully

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_binary - prints binary equal to decimal number
+ * @n: number to be printed in binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	int jm, num = 0;
+	unsigned long int jm1;
+
+	for (jm = 63; jm >= 0; jm--)
+	{
+		jm1 = n >> jm;
+
+		if (jm1 & 1)
+		{
+			putchar('1');
+			num++;
+		}
+		else if (num)
+			putchar('0');
+	}
+	if (!num)
+		putchar('0');
+}
