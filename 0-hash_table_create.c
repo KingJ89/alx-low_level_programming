55435bdf3d2554abbfde0ef9019597b0790524e6
KingJ89 <janmutewera@gmail.com>
2023-12-22 14:05:46 -0800

    task readme and 0 hash table create completed

---
+++ 0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table.
+ * @size: the size of the array
+ * Return: if error, NULL,oterwise - pointer to new hash table.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int j;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if  (!ht->array)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	for (j = 0; j < size; j++)
+		ht->array[j] = NULL;
+
+	return (ht);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+this is a non empty readme file on the alx project 0x1A ash tables by Jan mutewera
