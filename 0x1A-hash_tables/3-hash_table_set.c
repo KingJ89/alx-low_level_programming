75ea66a7c363da5f4ceb415e0477b1e9cd648fc0
KingJ89 <janmutewera@gmail.com>
2024-02-06 11:46:10 -0800

    task 3 amended

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,16 +11,42 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	if (!ht || !key || !*key)
-        return NULL;
+        return(0);
 
     unsigned long int index = key_index((const unsigned char *)key, ht->size);
     if (index >= ht->size)
-        return NULL;
-
+        return(0);
+    
     for (hash_node_t *node = ht->array[index]; node; node = node->next)
+    {
         if (strcmp(node->key, key) == 0)
-            return node->value;
+            return(!node->value);
 
-    return NULL;
+    return(0);
+    return(1);
+    }
 }
+
+/*Creates new node */
+hash_node_t *new_node = malloc(sizeof(hash_node_t));
+if (!new_node->key)
+{
+	free(new_node);
+	return(0);
+}
+
+new _node->value = strdup(value);
+if (!new_node->value)
+{
+	free(new_node->key);
+	free(new_node);
+	return(0);
+
+}
+
+/*inserts new node at beggining of the list */
+new_node->next = ht->array[index];
+ht->array[index] = new_node;
+
+return(1);
 
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -49,6 +49,8 @@ char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
-
+hash_node_t *new_node = malloc(sizeof(hash_node_t));
+hash_table_set(hash_table_t *ht, const char *key, const char *value)
+unsigned long int index = key_index((const unsigned char *)key, ht->size);
 #endif /* HASH_TABLES_H */
 
